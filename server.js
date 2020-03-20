0f7b2b73a1bc14f4a0efbc61d44ae551284e4c8b
mikevampm323 <mike.vartanyan@yahoo.com>
2020-03-19 22:00:32 -0700

    added routes

---
+++ server.js
@@ -0,0 +1,25 @@
+// Create a basic express server
+
+
+
+
+
+const express = require('express');
+const path = require("path");
+
+const app = express();
+
+let PORT = process.env.PORT || 8080;
+
+// Sets up the middleware for the  Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require('./routes/mockApiRoute')(app);
+require("./routes/htmlRoutes")(app);
+
+
+
+app.listen(PORT, () => {
+    console.log(`Running on PORT ${PORT}`);
+})
