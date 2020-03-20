0f94e3084f572adadce9c6f0c17f9628177a17f2
mikevampm323 <mike.vartanyan@yahoo.com>
2020-03-19 22:11:47 -0700

    server setup

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ let PORT = process.env.PORT || 8080;
 // Sets up the middleware for the  Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
-require('./routes/mockApiRoute')(app);
+// require('./routes/ApiRoutes')(app);
 require("./routes/htmlRoutes")(app);
 
 
